1ed592a01a5a13ceebe40d6613de047c5640cf2c
shchae04 <shchae04@naver.com>
2022-06-09 16:41:33 +0900

    Add User,BoardModify.

--- src/main/java/com/practice/board/controller/BoardController.java
+++ src/main/java/com/practice/board/controller/BoardController.java
@@ -8,7 +8,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BoardController {
@@ -73,6 +72,7 @@ public class BoardController {
         boardTemp.setTitle(board.getTitle());
         boardTemp.setContent(board.getContent());
        System.out.println(board.getTitle());
+        boardService.write(boardTemp);
 
         return "redirect:/board/list";
    }
---
+++ src/main/java/com/practice/board/service/UserService.java
@@ -0,0 +1,15 @@
+package com.practice.board.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Service
+public class UserService {
+
+
+    //회원가입 처리
+    public void register(){
+
+    }
+
+}
