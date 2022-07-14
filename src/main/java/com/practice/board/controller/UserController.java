fde260c348ab9cbbce340b29c41565fda9fabed6
shchae04 <shchae04@naver.com>
2022-07-14 16:46:04 +0900

    img file add

--- src/main/java/com/practice/board/controller/UserController.java
+++ src/main/java/com/practice/board/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.practice.board.controller;
 
+import com.practice.board.entity.User;
 import com.practice.board.service.UserService;
 import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -21,8 +23,11 @@ public class UserController {
     }
 
     @PostMapping("/register")
-    public void register(){
-
+    public void register(User user, Model model){
+        System.out.println("user정보 : " + user);
+        System.out.println("user 회원가입 요청");
+        // user 정보를 암호화 해야함 security 사용.
+        userService.register(user);
     }
 
     //login access 처리
@@ -35,6 +40,7 @@ public class UserController {
     @PostMapping("/login")
     public void makesession(HttpSession session){
         session.setAttribute("login","login_user");
+        String aar = (String)session.getAttribute("login");
     }
 
 
